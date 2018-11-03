cf525e12bc7723a1095a5479c9d5d6577530b150
DisposaBoy <disposaboy@kuroku.io>
2018-11-03 21:41:00 +0000

    add supports for 'unimported' packages
    * auto-completing `json.` will now try to import `encoding/json`
    * if that succeeds we can also optionally insert the import into the file
    * known bugs: when adding the import, the view will scroll
    * known limitation: we don't scan GOPATH and we don't support the vendor directory
    * related: https://github.com/KurokuLabs/margo/issues/75
    * related: https://github.com/KurokuLabs/margo/issues/62

--- golang/margocode.go
+++ golang/margocode.go
@@ -1,6 +1,7 @@
 package golang
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
@@ -15,6 +16,8 @@ import (
 	"margo.sh/mgpf"
 	"margo.sh/mgutil"
 	"math"
+	"os/exec"
+	"path"
 	"regexp"
 	"runtime"
 	"runtime/debug"
@@ -55,11 +58,16 @@ type marGocodeCtl struct {
 	mxQ *mgutil.ChanQ
 
 	mu     sync.RWMutex
+	cfg    MarGocodeCtl
 	pkgs   *mgcCache
 	cmdMap map[string]func(*mg.CmdCtx)
 	logs   *log.Logger
-	dbgPrn func(*types.Package) bool
-	mode   ImporterMode
+
+	ipbn struct {
+		sync.RWMutex
+		initOnce sync.Once
+		m        map[string]string
+	}
 }
 
 func (mgc *marGocodeCtl) importerFactories() (newDefaultImporter, newFallbackImporter importerFactory, srcMode bool) {
@@ -68,7 +76,7 @@ func (mgc *marGocodeCtl) importerFactories() (newDefaultImporter, newFallbackImp
 
 	s := mgc.newSrcImporter
 	b := mgc.newBinImporter
-	switch mgc.mode {
+	switch mgc.cfg.ImporterMode {
 	case SrcImporterWithFallback:
 		return s, b, true
 	case SrcImporterOnly:
@@ -78,6 +86,40 @@ func (mgc *marGocodeCtl) importerFactories() (newDefaultImporter, newFallbackImp
 	default:
 		panic("unreachable")
 	}
+}
+
+// importPathByName returns an import path whose pkg's name is pkgName
+func (mgc *marGocodeCtl) importPathByName(pkgName string) string {
+	// try the cache first
+	// it includes packages the user actually imported
+	// so there's theoretically a better chance of importing the ideal package
+	// in cases where there's a name collision
+	if p := mgc.ipbnFromCache(pkgName); p != "" {
+		return p
+	}
+
+	mgc.ipbn.RLock()
+	defer mgc.ipbn.RUnlock()
+	return mgc.ipbn.m[pkgName]
+}
+
+func (mgc *marGocodeCtl) ipbnFromCache(pkgName string) string {
+	mgc.mu.RLock()
+	defer mgc.mu.RUnlock()
+
+	importPath := ""
+	mgc.pkgs.forEach(func(e mgcCacheEnt) bool {
+		if p := e.Pkg; p.Name() == pkgName {
+			importPath = p.Path()
+			// presence of the vendor directory requires knowing
+			// where the srcDir of the import and we don't currently store that
+			if !strings.Contains(importPath, "/vendor/") {
+				return false
+			}
+		}
+		return true
+	})
+	return importPath
 }
 
 // newSrcImporter returns a new instance a source code importer
@@ -114,36 +156,34 @@ func (mgc *marGocodeCtl) autoPruneCache(mx *mg.Ctx) {
 	}
 
 	mgc.mu.RLock()
-	dpr := mgc.dbgPrn
+	dpr := mgc.cfg.DebugPrune
 	mgc.mu.RUnlock()
 
 	if dpr != nil {
-		mgc.pkgs.forEach(func(e mgcCacheEnt) {
+		mgc.pkgs.forEach(func(e mgcCacheEnt) bool {
 			if dpr(e.Pkg) {
 				mgc.pkgs.del(e.Key)
 			}
+			return true
 		})
 	}
 }
 
-func (mgc *marGocodeCtl) conf(mx *mg.Ctx, ctl *MarGocodeCtl) {
-	mgc.mu.Lock()
-	defer mgc.mu.Unlock()
+func (mgc *marGocodeCtl) configure(f func(*marGocodeCtl)) {
+	mctl.mu.Lock()
+	defer mctl.mu.Unlock()
 
-	if ctl.Debug {
-		mgc.logs = mx.Log.Dbg
-	}
-	mgc.dbgPrn = ctl.DebugPrune
-	mgc.mode = ctl.ImporterMode
+	f(mgc)
 }
 
 func newMarGocodeCtl() *marGocodeCtl {
 	mgc := &marGocodeCtl{}
 	mgc.pkgs = &mgcCache{m: map[mgcCacheKey]mgcCacheEnt{}}
 	mgc.cmdMap = map[string]func(*mg.CmdCtx){
-		"help":        mgc.helpCmd,
-		"cache-list":  mgc.cacheListCmd,
-		"cache-prune": mgc.cachePruneCmd,
+		"help":                mgc.helpCmd,
+		"cache-list":          mgc.cacheListCmd,
+		"cache-prune":         mgc.cachePruneCmd,
+		"unimported-packages": mgc.unimportedPackagesCmd,
 	}
 	mgc.mxQ = mgutil.NewChanQ(10)
 	go func() {
@@ -155,6 +195,12 @@ func newMarGocodeCtl() *marGocodeCtl {
 }
 
 func (mgc *marGocodeCtl) Reduce(mx *mg.Ctx) *mg.State {
+	if mx.LangIs(mg.Go) {
+		mgc.ipbn.initOnce.Do(func() {
+			go mgc.initIPBN(mx)
+		})
+	}
+
 	switch mx.Action.(type) {
 	case mg.RunCmd:
 		return mx.AddBuiltinCmds(mgc.cmds()...)
@@ -167,12 +213,53 @@ func (mgc *marGocodeCtl) Reduce(mx *mg.Ctx) *mg.State {
 	return mx.State
 }
 
+func (mgc *marGocodeCtl) initIPBN(mx *mg.Ctx) {
+	// TODO: scan GOPATH as well
+
+	cmd := exec.Command("go", "list", "-find", "-e", "-f={{.Name}},{{.ImportPath}}", "std")
+	out, err := cmd.Output()
+	if err != nil {
+		mx.Log.Printf("cannot load the list of stdlib packages", err)
+	}
+
+	mgc.ipbn.Lock()
+	defer mgc.ipbn.Unlock()
+
+	if mgc.ipbn.m == nil {
+		mgc.ipbn.m = map[string]string{}
+	}
+	m := mgc.ipbn.m
+
+	skip := map[string]bool{
+		"":         true,
+		"cmd":      true,
+		"internal": true,
+		"vendor":   true,
+		"main":     true,
+	}
+
+	scanner := bufio.NewScanner(bytes.NewReader(out))
+	for scanner.Scan() {
+		line := strings.SplitN(scanner.Text(), ",", 2)
+		if len(line) != 2 {
+			continue
+		}
+		nm, pth := line[0], line[1]
+		if nm == "" {
+			nm = path.Base(pth)
+		}
+		if !skip[nm] && !skip[strings.Split(pth, "/")[0]] {
+			m[nm] = pth
+		}
+	}
+}
+
 // srcMode returns true if the importMode is not SrcImporterOnly or SrcImporterWithFallback
 func (mgc *marGocodeCtl) srcMode() bool {
 	mgc.mu.RLock()
 	defer mgc.mu.RUnlock()
 
-	switch mgc.mode {
+	switch mgc.cfg.ImporterMode {
 	case SrcImporterOnly, SrcImporterWithFallback:
 		return true
 	case BinImporterOnly:
@@ -297,6 +384,41 @@ func (mgc *marGocodeCtl) cacheListCmd(cx *mg.CmdCtx) {
 	tbw.Write(buf.Bytes())
 }
 
+func (mgc *marGocodeCtl) unimportedPackagesCmd(cx *mg.CmdCtx) {
+	defer cx.Output.Close()
+
+	type ent struct {
+		nm  string
+		pth string
+	}
+
+	mgc.ipbn.RLock()
+	ents := make([]ent, 0, len(mgc.ipbn.m))
+	for nm, pth := range mgc.ipbn.m {
+		ents = append(ents, ent{nm: nm, pth: pth})
+	}
+	mgc.ipbn.RUnlock()
+
+	buf := &bytes.Buffer{}
+	tbw := tabwriter.NewWriter(cx.Output, 1, 4, 1, ' ', 0)
+	defer tbw.Flush()
+
+	digits := int(math.Floor(math.Log10(float64(len(ents)))) + 1)
+	sfxFormat := "\t%s\t%s\n"
+	hdrFormat := "%s" + sfxFormat
+	rowFormat := fmt.Sprintf("%%%dd/%d", digits, len(ents)) + sfxFormat
+
+	sort.Slice(ents, func(i, j int) bool {
+		return ents[i].nm < ents[j].nm
+	})
+
+	fmt.Fprintf(buf, hdrFormat, "Count:", "Name:", "ImportPath:")
+	for i, e := range ents {
+		fmt.Fprintf(buf, rowFormat, i+1, e.nm, e.pth)
+	}
+	tbw.Write(buf.Bytes())
+}
+
 func (mgc *marGocodeCtl) cachePruneCmd(cx *mg.CmdCtx) {
 	defer cx.Output.Close()
 
@@ -329,7 +451,17 @@ func (mgc *marGocodeCtl) helpCmd(cx *mg.CmdCtx) {
 	cx.Output.Write([]byte(`Usage: ` + cx.Name + ` $subcmd [args...]
 	cache-prune [regexp, or path...] - remove packages matching glob from the cache. default: '.*'
 	cache-list                       - list cached packages, see '` + cx.Name + ` cache-list --help' for more details
+	unimported-packages              - list the packages for the UnimportedPackages feature
 `))
+}
+
+func (mgc *marGocodeCtl) newGcSuggest(mx *mg.Ctx) *gcSuggest {
+	mgc.mu.RLock()
+	defer mgc.mu.RUnlock()
+
+	gsu := &gcSuggest{cfg: mgc.cfg}
+	gsu.imp = gsu.newGsuImporter(mx)
+	return gsu
 }
 
 func (mgc *marGocodeCtl) cmd(cx *mg.CmdCtx) *mg.State {
@@ -362,10 +494,33 @@ type MarGocodeCtl struct {
 	// The mode in which the types.Importer shouler operate
 	// By default it is SrcImporterWithFallback
 	ImporterMode ImporterMode
+
+	// Don't try to automatically import packages when auto-compeltion fails
+	// e.g. when `json.` is typed, if auto-complete fails
+	// "encoding/json" is imported and auto-complete attempted on that package instead
+	// See AddUnimportedPackages
+	NoUnimportedPackages bool
+
+	// If a package was imported internally for use in auto-completion,
+	// insert it in the source code
+	// See NoUnimportedPackages
+	// e.g. after `json.` is typed, `import "encoding/json"` added to the code
+	AddUnimportedPackages bool
+
+	// Don't propose builtin types and functions
+	NoBuiltins bool
+
+	// Whether or not to propose builtin types and functions
+	ProposeTests bool
 }
 
 func (mgc *MarGocodeCtl) RInit(mx *mg.Ctx) {
-	mctl.conf(mx, mgc)
+	mctl.configure(func(m *marGocodeCtl) {
+		m.cfg = *mgc
+		if mgc.Debug {
+			m.logs = mx.Log.Dbg
+		}
+	})
 }
 
 func (mgc *MarGocodeCtl) Reduce(mx *mg.Ctx) *mg.State {
