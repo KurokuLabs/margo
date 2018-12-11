778beade6b26f4bd2ce4077f0195e814bb321753
DisposaBoy <disposaboy@kuroku.io>
2018-12-11 14:47:22 +0000

    implement gopkg.ImportDir using KVMap.Memo

--- golang/gopkg/import.go
+++ golang/gopkg/import.go
@@ -1,27 +1,34 @@
 package gopkg
 
 import (
+	"go/build"
 	"margo.sh/golang/goutil"
 	"margo.sh/mg"
 	"margo.sh/vfs"
 	"path/filepath"
 )
 
-func ImportDir(mx *mg.Ctx, path string) (*Pkg, error) {
-	path = filepath.Clean(path)
-
-	kv, err := vfs.Root.KV(path)
+func ImportDir(mx *mg.Ctx, dir string) (*Pkg, error) {
+	dir = filepath.Clean(dir)
+	nd, kv, err := vfs.Root.KV(dir)
 	if err != nil {
 		return nil, err
 	}
-
-	type K struct{ path string }
-	k := K{path}
-	if p, ok := kv.Get(k).(*Pkg); ok {
-		return p, nil
+	if !nd.SomeSuffix(".go") {
+		return nil, &build.NoGoError{Dir: dir}
 	}
 
-	bpkg, err := goutil.BuildContext(mx).ImportDir(path, 0)
+	type K struct{}
+	v, err := kv.Memo(K{}, func() (interface{}, error) {
+		p, err := importDir(mx, dir)
+		return p, err
+	})
+	p, _ := v.(*Pkg)
+	return p, err
+}
+
+func importDir(mx *mg.Ctx, dir string) (*Pkg, error) {
+	bpkg, err := goutil.BuildContext(mx).ImportDir(dir, 0)
 	if err != nil {
 		return nil, err
 	}
@@ -33,6 +40,5 @@ func ImportDir(mx *mg.Ctx, path string) (*Pkg, error) {
 		Standard:   bpkg.Goroot,
 	}
 	p.Finalize()
-	kv.Put(k, p)
 	return p, nil
 }
